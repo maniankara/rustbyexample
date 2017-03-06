c97892ee882ba3b272829ff94df19afc0c56de54
anoop <anoop@tuxrea.com>
2017-03-06 23:28:15 +0200

    added iterator_any

---
+++ tutorial/12iterator_any.rs
@@ -0,0 +1,34 @@
+/*
+pub trait Iterator {
+
+    type Item;
+
+    // `any` takes `&mut self` meaning the caller may be borrowed
+    // and modified, but not consumed.
+    fn any<F>(&mut self, f. F) -> bool where
+        // `FnMut` meaning any captured variable may at most be
+        // modified, not consumed. `Self::Item` states it takes
+        // arguments to the closure by value.
+        F: FnMut(Self::Item) -> bool {}
+
+
+ }
+
+
+*/
+
+fn main() {
+
+    let vec1 = vec![1,2,3];
+    let vec2 = vec![4,8,9];
+
+    println!("2 is present: {}", vec1.iter().any(|&x| x==2));
+    println!("2 is present: {}", vec2.into_iter().any(|x| x==2));
+
+    let arr1 = [1,2,3];
+    let arr2 = [4,8,9];
+
+    println!("2 is present: {}", arr1.into_iter().any(|&x| x==2)); // still yields &i32
+    println!("2 is present: {}", arr2.iter().any(|&x| x==2));
+
+}
