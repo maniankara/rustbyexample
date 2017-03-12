7c27e3af4c2e979a7a63fc69e3eba573473c2c9d
anoop <anoop@tuxrea.com>
2017-03-12 13:44:27 +0200

    added ownerships

---
+++ tutorial/20ownership.rs
@@ -0,0 +1,25 @@
+fn print_block(c: Box<i32>) {
+    println!("value of c is: {}",c);
+}
+
+
+fn main() {
+    let x = 10i32;
+    let y = x;
+
+    //Stack vars
+    println!("Value of x is: {}" ,x);
+    println!("Value of y is: {}", y);
+
+    // Heap vars
+    let a = Box::new(5i32);
+    println!("Value of a is: {}",a);
+
+    let b = a;
+    // println!("Value of a is: {}",a); // value a used here after move
+
+    print_block(b); // b is moved to print_block
+
+    // println!("Value of a is: {}",b); // value b used here after move
+
+}
