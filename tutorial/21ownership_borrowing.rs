a3540c6a220d71d3a665c932ae5510aeadc6a26c
anoop <anoop@tuxrea.com>
2017-03-12 14:20:06 +0200

    borrowing with active references

--- tutorial/21ownership_borrowing.rs
+++ tutorial/21ownership_borrowing.rs
@@ -16,15 +16,27 @@ fn borrow_i32(j: &i32) {
 
 fn main() {
 
-    // box
-    let box_i32 = Box::new(5i32);
-    borrow_box_i32(&box_i32);
-    eat_box_i32(box_i32);
-    // println!("Try to print box: {}", box_i32);
-
     let stack_i32 = 5i32;
     borrow_i32(&stack_i32);
     eat_i32(stack_i32);
     // println!("Try to print i32: ", stack_i32);
+
+    // box
+    let box_i32 = Box::new(5i32);
+    borrow_box_i32(&box_i32);
+
+
+    // eat while ref exists
+    {
+
+        let _tmp : &i32 = &box_i32;
+        // eat_box_i32(box_i32); // error as ref exists
+
+        eat_i32(stack_i32); // Works perfect !! 'cos its stack and not box (heap) and inside a block
+    }
+
+    eat_box_i32(box_i32);
+    // println!("Try to print box: {}", box_i32);
+
 
 }
