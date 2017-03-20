9efda4895dc3a676602bf44db7bbdc6c6913ce76
anoop <anoop@tuxrea.com>
2017-03-20 22:34:04 +0200

    simple thread

---
+++ tutorial/26std/39thread.rs
@@ -0,0 +1,21 @@
+use std::thread;
+
+static NTHREADS: i32 = 10;
+
+fn main() {
+
+    let mut children = vec![];
+
+    for i in 0..NTHREADS {
+        children.push(
+            thread::spawn(move || {
+                println!("Thread no: {}", i);
+            })
+        );
+    }
+
+    for c in children {
+        let _ = c.join();
+    }
+
+}
