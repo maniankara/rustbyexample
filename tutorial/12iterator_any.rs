67e50d1023ef34eee78a945e7f89e6bb697d7f5a
anoop <anoop@tuxrea.com>
2017-04-08 23:26:41 +0300

    added map

--- tutorial/12iterator_any.rs
+++ tutorial/12iterator_any.rs
@@ -19,16 +19,19 @@ pub trait Iterator {
 
 fn main() {
 
-    let vec1 = vec![1,2,3];
-    let vec2 = vec![4,8,9];
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 8, 9];
 
-    println!("2 is present: {}", vec1.iter().any(|&x| x==2));
-    println!("2 is present: {}", vec2.into_iter().any(|x| x==2));
+    println!("2 is present: {}", vec1.iter().any(|&x| x == 2));
+    println!("2 is present: {}", vec2.into_iter().any(|x| x == 2));
 
-    let arr1 = [1,2,3];
-    let arr2 = [4,8,9];
+    let arr1 = [1, 2, 3];
+    let arr2 = [4, 8, 9];
 
-    println!("2 is present: {}", arr1.into_iter().any(|&x| x==2)); // still yields &i32
-    println!("2 is present: {}", arr2.iter().any(|&x| x==2));
+    println!("2 is present: {}", arr2.iter().any(|&x| x == 2));
+    println!("2 is present: {}", arr1.into_iter().any(|&x| x == 2)); // still yields &i32
 
+    // tmp
+
+    println!("first value: {:?}", vec1.get(0).map(|i| i).unwrap());
 }
